05c5414f406240c72e3c51cb48ab0abb71ef5605
Ankush <ankush357159@gmail.com>
2021-12-30 18:01:59 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { AddMenu } from "./components/AddMenu";
+import { MenuList } from "./components/MenuList";
+import { UpdateMenu } from "./components/UpdateMenu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav className='navbar navbar-expand navbar-dark bg-info'>
+          <a href='/' className='navbar-brand mx-5'>
+            Restaurant Menu
+          </a>
+          <div className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <Link to={"/add/"} className='nav-link'>
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+        <div className='container m-10'>
+          {/* Add routes */}
+          <Switch>
+            <Route exact path={["/", "/menus"]} component={MenuList} />
+            <Route exact path='/add/' component={AddMenu} />
+            <Route path='/menu/:id/update/' component={UpdateMenu} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
